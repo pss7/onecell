fea29771611859a8609193c616814f667cc4723f
parksangseok <park0157@naver.com>
2025-11-27 08:53:11 +0900

    commit

--- js/landing.js
+++ js/landing.js
@@ -222,6 +222,49 @@ $(function () {
     centerMode: true,
   });
 
+  /* 팝업 */
+  $('.doctor01').click(function () {
+    $('#popupWrap01').addClass('active');
+  });
+  $('#popupWrap01 .popupCloseBtn').click(function () {
+    $('#popupWrap01').removeClass('active');
+  });
+
+  $('.doctor02').click(function () {
+    $('#popupWrap02').addClass('active');
+  });
+  $('#popupWrap02 .popupCloseBtn').click(function () {
+    $('#popupWrap02').removeClass('active');
+  });
+
+  $('.doctor03').click(function () {
+    $('#popupWrap03').addClass('active');
+  });
+  $('#popupWrap03 .popupCloseBtn').click(function () {
+    $('#popupWrap03').removeClass('active');
+  });
+
+  $('.doctor04').click(function () {
+    $('#popupWrap04').addClass('active');
+  });
+  $('#popupWrap04 .popupCloseBtn').click(function () {
+    $('#popupWrap04').removeClass('active');
+  });
+
+  $('.doctor05').click(function () {
+    $('#popupWrap05').addClass('active');
+  });
+  $('#popupWrap05 .popupCloseBtn').click(function () {
+    $('#popupWrap05').removeClass('active');
+  });
+
+  $('.doctor06').click(function () {
+    $('#popupWrap06').addClass('active');
+  });
+  $('#popupWrap06 .popupCloseBtn').click(function () {
+    $('#popupWrap06').removeClass('active');
+  });
+
   /* 푸터 */
   $('#footerWrap .consulting').click(function () {
     if ($(this).hasClass('active')) {
